74001ef579aafb9d0aa8d375bbad0ed64bf0ed84
Birol ÖZTÜRK <70208466+zBlackOPS@users.noreply.github.com>
2021-05-13 20:24:15 +0300

    Delete 4thDay_3rdHomeWork directory

--- 4thDay_3rdHomeWork/src/abstracts/CustomerCheckService.java
+++
@@ -1,9 +0,0 @@
-package abstracts;
-
-import entities.Customer;
-
-public interface CustomerCheckService {
-
-    boolean checkIfRealPerson(Customer customer) throws Exception;
-
-}
--- 4thDay_3rdHomeWork/src/concrete/NeroCustomerManager.java
+++
@@ -1,6 +0,0 @@
-package concrete;
-
-import abstracts.BaseCustomerManager;
-
-public class NeroCustomerManager extends BaseCustomerManager {
-}
