ecda23f2fcc0df1a00fd9c6505037625a4129334
Birol ÖZTÜRK <70208466+zBlackOPS@users.noreply.github.com>
2021-05-13 11:34:29 +0300

    Update Main.java

--- 4thDay_3rdHomeWork/src/Main.java
+++ 4thDay_3rdHomeWork/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BaseCustomerManager customerManager=new StarbucksCustomerManager(new MernisServiceAdepters());
-        customerManager.save(new Customer(1, "Birol", "ÖZTÜRK", "10186742526", LocalDate.of(2006, 8, 7)));
+        customerManager.save(new Customer(1, "Birol", "ÖZTÜRK", "0", LocalDate.of(2000, 8, 7)));
     }
 
 }
